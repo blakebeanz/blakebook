b76cf1131cd3ab5b0895f1bf9742be3771c9ab59
Blake Haselberger <blake.haselberger@avanade.com>
2023-04-30 16:08:19 -0700

    added all components: sidebar, topbar, rightbar, feed, closeFriend, post, etc.

--- src/pages/home/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Person } from '@mui/icons-material'
-import Topbar from '../../components/topbar/Topbar'
-
-function Home() {
-  return <Topbar />
-}
-
-export default Home
