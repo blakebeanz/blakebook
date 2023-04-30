ef1411b080e53921349a675b2b654fd19bd32070
Blake Haselberger <blake.haselberger@avanade.com>
2023-04-30 14:48:19 -0700

    added topbar

---
+++ src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Person } from '@mui/icons-material'
+import Topbar from '../../components/topbar/Topbar'
+
+function Home() {
+  return <Topbar />
+}
+
+export default Home
